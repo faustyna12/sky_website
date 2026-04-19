341b2f9d7f18ea69a7992594b2efd2581dd3fe75
Hikmat <2gshoppp@gmail.com>
2026-04-20 03:02:15 +0400

    Implement team list view with filtering and URLs

--- teams_app/views.py
+++ teams_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Team, Dependency
 
-# Create your views here.
+def team_list(request):
+    query = request.GET.get('q', '')
+    teams = Team.objects.all()
+    
+    if query:
+        teams = teams.filter(name__icontains=query)
+    
+    for team in teams:
+        team.upstream = Dependency.objects.filter(source_team=team, dependency_type='Upstream')
+        team.downstream = Dependency.objects.filter(target_team=team, dependency_type='Downstream')
+        team.schedule_url = reverse('schedule_meeting') + f'?team={team.id}'
+    
+    context = {
+        'teams': teams,
+        'query': query,
+    }
+    return render(request, 'teams_app/teams.html', context)
