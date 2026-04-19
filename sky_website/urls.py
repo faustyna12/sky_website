f04b4d309dc6306fa9dc26723bd50191526f185e
Hikmat <2gshoppp@gmail.com>
2026-04-20 03:42:49 +0400

    Update URL patterns for login and teams
    Rearranged login path and added teams path.

--- sky_website/urls.py
+++ sky_website/urls.py
@@ -5,8 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('organisation/', include('organisation_app.urls')),
-    
-        path('login/', auth_views.LoginView.as_view(template_name='login_app/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('teams/', include('teams_app.urls')),
 ]
-    #path('dashboard/', include('sky_dash.urls')),# 
