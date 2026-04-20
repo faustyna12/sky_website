74a065d09fbee80b6e29a25583f55369b4bf0486
Hikmat <2gshoppp@gmail.com>
2026-04-20 04:31:34 +0400

    Update views.py

--- teams_app/views.py
+++ teams_app/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
-from django.urls import reverse
-from .models import Team, Dependency
+from .models import Team
 
 def team_list(request):
-    query = request.GET.get('q', '')
     teams = Team.objects.all()
     
-    if query:
-        teams = teams.filter(name__icontains=query)
+    search = request.GET.get('search')
+    skill = request.GET.get('skill')
     
-    for team in teams:
-        team.upstream = Dependency.objects.filter(source_team=team, dependency_type='Upstream')
-        team.downstream = Dependency.objects.filter(target_team=team, dependency_type='Downstream')
-        team.schedule_url = reverse('schedule_meeting') + f'?team={team.id}'
+    if search:
+        teams = teams.filter(name__icontains=search)
     
-    context = {
-        'teams': teams,
-        'query': query,
-    }
-    return render(request, 'teams_app/teams.html', context)
+    if skill:
+        teams = teams.filter(skills__name__icontains=skill)
+    
+    return render(request, 'teams_app/teams.html', {'teams': teams})
