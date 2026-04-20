62d223aa9a4e27898d4121a0b256210f21d21103
Hikmat <2gshoppp@gmail.com>
2026-04-20 16:37:11 +0400

    Update models.py

--- teams_app/models.py
+++ teams_app/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Skill(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
-
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(blank=True, null=True)
-
-    members = models.ManyToManyField(User, blank=True)
-    skills = models.ManyToManyField(Skill, blank=True)
-
-    upstream = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        blank=True,
-        related_name='downstream'
-    )
+    description = models.TextField(blank=True)
+    department = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    members = models.ManyToManyField(User, blank=True, related_name='teams')
+    skills = models.ManyToManyField(Skill, blank=True, related_name='teams')
+    repositories = models.IntegerField(default=0)
+    upstream = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='downstream')
+    calendar_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def member_count(self):
+        return self.members.count()
