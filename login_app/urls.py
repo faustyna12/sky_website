bd21144e94cc14892906b09d20d46e4238099b95
rafia malik <w2069502@westminster.ac.uk>
2026-04-27 17:26:13 +0100

    Help and terms added to login page

--- login_app/urls.py
+++ login_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    # This maps to http://127.0.0.1:8000/login/
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    # In your urls.py
+    # Login Page
+    path('', auth_views.LoginView.as_view(template_name='login_app/login.html'), name='login'),
+
+    # Help Page (Matches line 102 in your error)
+    path('help/', TemplateView.as_view(template_name='login_app/help.html'), name='help'),
+
+    # Contact/Support Page (Matches line 103 in your error)
+    path('contact_support/', TemplateView.as_view(template_name='login_app/contact.html'), name='contact_support'),
+
+   
 ]
