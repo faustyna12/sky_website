cca403565b561bde93dd09e1591fbe22df436bf3
yonis123456 <w2116941@westminster.ac.uk>
2026-04-29 22:07:41 +0100

    Added teams list and search to reports

--- reports_app/views.py
+++ reports_app/views.py
@@ -9,15 +9,24 @@ def reports_dashboard(request):
     if not request.user.is_staff:
         return redirect('dashboard')
     
+    search = request.GET.get('search', '')
+    
     total_users = User.objects.count()
     total_teams = Team.objects.count()
     total_departments = Department.objects.count()
     recent_users = User.objects.order_by('-date_joined')[:5]
+    
+    if search:
+        all_teams = Team.objects.filter(name__icontains=search)
+    else:
+        all_teams = Team.objects.all()
     
     context = {
         'total_users': total_users,
         'total_teams': total_teams,
         'total_departments': total_departments,
         'recent_users': recent_users,
+        'all_teams': all_teams,
+        'search': search,
     }
     return render(request, 'reports_app/reports.html', context)
