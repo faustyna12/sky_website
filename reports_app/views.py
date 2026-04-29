99c7bcba0d367341b21f006992109ec758f9e459
yonis123456 <w2116941@westminster.ac.uk>
2026-04-29 21:25:41 +0100

    Added reports dashboard page

--- reports_app/views.py
+++ reports_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+def reports_dashboard(request):
+    total_users = User.objects.count()
+    context = {
+        'total_users': total_users,
+    }
+    return render(request, 'reports_app/reports.html', context)
